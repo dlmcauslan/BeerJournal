6f3c8d2b3f8c2c3920753466c3f487083a1400ce
dlmcauslan <dlmcauslan@gmail.com>
2018-06-11 21:17:42 +1200

    Removed old database files

--- app/src/main/java/com/wordpress/excelenteadventura/beerjournal/database/BeerDataBase.kt
+++ app/src/main/java/com/wordpress/excelenteadventura/beerjournal/database/BeerDataBase.kt
@@ -7,7 +7,7 @@ import android.content.Context
 import android.util.Log
 import com.wordpress.excelenteadventura.beerjournal.BeerRepository
 
-@Database(entities = arrayOf(Beer::class), version = 1)
+@Database(entities = [(Beer::class)], version = 1)
 abstract class BeerDataBase: RoomDatabase() {
 
     abstract fun beerDao(): BeerDao
@@ -23,33 +23,11 @@ abstract class BeerDataBase: RoomDatabase() {
                     // Create the database
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
                             BeerDataBase::class.java,
-                            "beer.db").addCallback(roomDatabaseCallback).build()
+                            "beer.db").build()
                     Log.d(LOG_TAG, "Creating the database")
                 }
             }
             return INSTANCE!!
         }
-
-        private val roomDatabaseCallback = object : RoomDatabase.Callback() {
-
-//            override fun onOpen(db: SupportSQLiteDatabase) {
-//                super.onOpen(db)
-//                PopulateDbAsync(INSTANCE).execute()
-//            }
-        }
-
-//        private class PopulateDbAsync(db: BeerDataBase?) : AsyncTask<Void, Void, Void>() {
-//
-//            private val beerDao: BeerDao = db!!.beerDao()
-//
-//            override fun doInBackground(vararg params: Void): Void? {
-//                // insert some dummy data
-//                var beer = Beer(name = "Beer1")
-//                beerDao.insertBeer(beer)
-//                beer = Beer(name = "Beer 2")
-//                beerDao.insertBeer(beer)
-//                return null
-//            }
-//        }
     }
 }
